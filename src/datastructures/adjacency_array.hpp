27ac695a2513780a9cb7393e642127918d9bb97d
Noah Wahl <noah.ares@yahoo.de>
2022-07-15 15:22:45 +0200

    JP: support MSF

--- src/datastructures/adjacency_array.hpp
+++ src/datastructures/adjacency_array.hpp
@@ -1,36 +1,31 @@
 #include "../../includes/definitions.hpp"
+
+#include <algorithm>
 #include <iostream>
 
 class AdjacencyArray {
   public:
-    using EdgeIterator = std::vector<algen::VertexId>::const_iterator;
+    using EdgeIterator = std::vector<std::pair<algen::VertexId, algen::Weight>>::const_iterator;
 
     explicit AdjacencyArray(const algen::WEdgeList &edges, const algen::VertexId num_vertices) {
         _indices.resize(num_vertices + 1);
         _edge_endpoints.resize(edges.size());
-        _weights.resize(edges.size());
+        std::vector<bool> vertex_reachable(num_vertices, false);
 
         std::vector<size_t> edge_count(num_vertices, 0);
-        for(const auto &e : edges) {
+        for (const auto &e : edges) {
             edge_count[e.tail]++;
+            vertex_reachable[e.head] = true;
+            vertex_reachable[e.tail] = true;
         }
-        for(size_t i = 1; i <= num_vertices; ++i) {
+        for (size_t i = 1; i <= num_vertices; ++i) {
             _indices[i] = _indices[i - 1] + edge_count[i - 1];
         }
-        for(const auto &e : edges) {
+        for (const auto &e : edges) {
             const size_t idx = _indices[e.tail + 1] - edge_count[e.tail]--;
-            _edge_endpoints[idx] = e.head;
-            _weights[idx] = e.weight;
+            _edge_endpoints[idx] = std::make_pair(e.head, e.weight);
         }
-
-        for (const auto i : _indices) {
-          std::cout << i << std::endl;
-        }
-        for (const auto i : _edge_endpoints) {
-          std::cout << i << std::endl;
-        }
-
-        // TODO: maybe sort by edge weight <noahares>
+        _num_reachable_vertices = std::count(vertex_reachable.begin(), vertex_reachable.end(), true);
     }
 
     EdgeIterator beginEdges(const algen::VertexId v) const {
@@ -41,12 +36,13 @@ class AdjacencyArray {
         return _edge_endpoints.begin() + _indices[v + 1];
     }
 
-    algen::Weight edgeWeight(EdgeIterator it) const {
-        return _weights[std::distance(_edge_endpoints.begin(), it)];
+
+    algen::VertexId numReachableVertices() const {
+        return _num_reachable_vertices;
     }
 
   private:
     std::vector<std::size_t> _indices;
-    std::vector<algen::VertexId> _edge_endpoints;
-    std::vector<algen::Weight> _weights;
+    std::vector<std::pair<algen::VertexId, algen::Weight>> _edge_endpoints;
+    algen::VertexId _num_reachable_vertices;
 };
--- src/jarnik_prim.cpp
+++ src/jarnik_prim.cpp
@@ -1,45 +1,64 @@
 #include "jarnik_prim.h"
 
-#include <utility>
+#include <algorithm>
 #include <iostream>
+#include <utility>
 
 algen::WEdgeList JarnikPrim::operator()(const algen::WEdgeList &edge_list, const algen::VertexId num_vertices) {
     const GraphRepresentation graph(edge_list, num_vertices);
     PriorityQueue pq;
     std::vector<algen::Weight> best_weights(num_vertices, W_INF);
-    std::vector<bool> in_mst(num_vertices, false);
+    std::vector<bool> in_msf(num_vertices, false);
     std::vector<algen::VertexId> parents(num_vertices, -1);
-    algen::WEdgeList mst;
-    mst.reserve(num_vertices - 1);
+    algen::WEdgeList msf;
+    algen::VertexId num_msf_edges = graph.numReachableVertices() - 1;
+    msf.reserve(num_msf_edges);
 
-    algen::VertexId root = 0; // TODO: random would be better <noahares>
-    pq.push(std::make_pair(0, root));
-    best_weights[root] = 0;
-    parents[root] = root;
+    auto root_edge = edge_list.begin();
+    while (msf.size() < num_msf_edges) {
+        algen::VertexId root = root_edge->tail;
+        std::cout << root << std::endl;
+        best_weights[root] = 0;
+        in_msf[root] = true;
+        parents[root] = root;
+        for (auto it = graph.beginEdges(root); it != graph.endEdges(root); ++it) {
+            const algen::VertexId v = it->first;
+            const algen::Weight w = it->second;
+            best_weights[v] = w;
+            parents[v] = root;
+            pq.push(std::make_pair(w, v));
+        }
 
-    while(!pq.empty() && mst.size() < num_vertices) {
-        const algen::VertexId u = pq.top().second;
-        pq.pop();
-        if(in_mst[u])
-            continue;
+        while (!pq.empty() && msf.size() < num_msf_edges) {
+            const algen::VertexId u = pq.top().second;
+            pq.pop();
+            if (in_msf[u])
+                continue;
 
-        mst.emplace_back(parents[u], u, best_weights[u]);
-        in_mst[u] = true;
-        for(auto it = graph.beginEdges(u); it != graph.endEdges(u); ++it) {
-            const algen::VertexId v = *it;
-            const algen::Weight w = graph.edgeWeight(it);
+            msf.emplace_back(parents[u], u, best_weights[u]);
+            in_msf[u] = true;
+            for (auto it = graph.beginEdges(u); it != graph.endEdges(u); ++it) {
+                const algen::VertexId v = it->first;
+                const algen::Weight w = it->second;
 
-            if(!in_mst[v] && best_weights[v] > w) {
-                best_weights[v] = w;
-                parents[v] = u;
-                pq.push(std::make_pair(w, v));
+                if (!in_msf[v] && best_weights[v] > w) {
+                    best_weights[v] = w;
+                    parents[v] = u;
+                    pq.push(std::make_pair(w, v));
+                }
             }
         }
+
+        root_edge = std::find_if(edge_list.begin(), edge_list.end(), [&in_msf](const auto &e) { return !in_msf[e.tail]; });
+        if (root_edge == edge_list.end())
+            break;
+        num_msf_edges--;
     }
 
-    for (const auto& e : mst) {
-      std::cout << e.tail << "," << e.head << std::endl;
-    }
+    // for (const auto& e : msf) {
+    //   std::cout << e.tail << "," << e.head << " ";
+    // }
+    // std::cout << std::endl;
 
-    return mst;
+    return msf;
 }
