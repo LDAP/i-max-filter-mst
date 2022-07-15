80f3ac1d2276eb00637f4ef2d01823d9302f9187
Noah Wahl <noah.ares@yahoo.de>
2022-07-15 15:23:04 +0200

    JP: Test MSF

--- tests/jarnik_prim_test.cc
+++ tests/jarnik_prim_test.cc
@@ -1,34 +1,69 @@
+#include "../includes/definitions.hpp"
+#include "../src/jarnik_prim.h"
+
 #include <algorithm>
 #include <gtest/gtest.h>
-#include "../src/jarnik_prim.h"
-#include "../includes/definitions.hpp"
 
 using ::testing::Test;
 
-TEST(JarnikPrimTests, correctness) {
-  algen::WEdgeList edges;
-  edges.emplace_back(0, 1, 3);
-  edges.emplace_back(0, 2, 1);
-  edges.emplace_back(0, 3, 10);
-  edges.emplace_back(2, 1, 1);
-  edges.emplace_back(1, 3, 1);
-  edges.emplace_back(3, 4, 1);
-  edges.emplace_back(2, 5, 5);
-  std::random_shuffle(edges.begin(), edges.end());
-  JarnikPrim jp;
-  auto mst = jp(edges, 6);
+TEST(JarnikPrimTests, mst_correctness) {
+    algen::WEdgeList edges;
+    edges.emplace_back(0, 1, 3);
+    edges.emplace_back(0, 2, 1);
+    edges.emplace_back(0, 3, 10);
+    edges.emplace_back(2, 1, 1);
+    edges.emplace_back(1, 3, 1);
+    edges.emplace_back(3, 4, 1);
+    edges.emplace_back(2, 5, 5);
+    JarnikPrim jp;
+    auto mst = jp(edges, 6);
 
-  algen::WEdgeList correct_mst;
-  correct_mst.emplace_back(0, 0, 0);
-  correct_mst.emplace_back(0, 2, 1);
-  correct_mst.emplace_back(2, 1, 1);
-  correct_mst.emplace_back(1, 3, 1);
-  correct_mst.emplace_back(3, 4, 1);
-  correct_mst.emplace_back(2, 5, 5);
-  // auto mst = correct_mst;
-  for (std::size_t i = 0; i < 6; ++i) {
-    ASSERT_EQ(mst[i].tail, correct_mst[i].tail);
-    ASSERT_EQ(mst[i].head, correct_mst[i].head);
-    ASSERT_EQ(mst[i].weight, correct_mst[i].weight);
-  }
+    algen::WEdgeList correct_mst;
+    correct_mst.emplace_back(0, 2, 1);
+    correct_mst.emplace_back(2, 1, 1);
+    correct_mst.emplace_back(1, 3, 1);
+    correct_mst.emplace_back(3, 4, 1);
+    correct_mst.emplace_back(2, 5, 5);
+    // auto mst = correct_mst;
+    for (std::size_t i = 0; i < 5; ++i) {
+        ASSERT_EQ(mst[i].tail, correct_mst[i].tail);
+        ASSERT_EQ(mst[i].head, correct_mst[i].head);
+        ASSERT_EQ(mst[i].weight, correct_mst[i].weight);
+    }
+};
+
+TEST(JarnikPrimTests, msf_correctness) {
+    algen::WEdgeList edges;
+    edges.emplace_back(0, 1, 3);
+    edges.emplace_back(0, 2, 1);
+    edges.emplace_back(0, 3, 10);
+    edges.emplace_back(2, 1, 1);
+    edges.emplace_back(1, 3, 1);
+    edges.emplace_back(3, 4, 1);
+    edges.emplace_back(2, 5, 5);
+
+    edges.emplace_back(9, 10, 1);
+    edges.emplace_back(9, 11, 3);
+    edges.emplace_back(10, 12, 2);
+
+    edges.emplace_back(42, 43, 1);
+
+    JarnikPrim jp;
+    auto mst = jp(edges, 44);
+
+    algen::WEdgeList correct_mst;
+    correct_mst.emplace_back(0, 2, 1);
+    correct_mst.emplace_back(2, 1, 1);
+    correct_mst.emplace_back(1, 3, 1);
+    correct_mst.emplace_back(3, 4, 1);
+    correct_mst.emplace_back(2, 5, 5);
+    correct_mst.emplace_back(9, 10, 1);
+    correct_mst.emplace_back(10, 12, 2);
+    correct_mst.emplace_back(9, 11, 3);
+    correct_mst.emplace_back(42, 43, 1);
+    for (std::size_t i = 0; i < 9; ++i) {
+        ASSERT_EQ(mst[i].tail, correct_mst[i].tail);
+        ASSERT_EQ(mst[i].head, correct_mst[i].head);
+        ASSERT_EQ(mst[i].weight, correct_mst[i].weight);
+    }
 };
