655e0ae94a81ed39f19eac50d6edb5bb8a9696d7
Noah Wahl <noah.ares@yahoo.de>
2022-07-17 13:33:14 +0200

    IMaxFilter: fix because LSP died

--- src/i_max_filter.hpp
+++ src/i_max_filter.hpp
@@ -18,14 +18,15 @@ class IMaxFilter {
         std::vector<algen::Weight> rmq_weights(msf.size());
         std::vector<std::size_t> jp_num(num_vertices, inf);
         std::size_t i = 0;
+        jp_num[msf.front().tail] = i++;
         for (const auto &e : msf) {
-            if (jp_num[e.tail] == inf)
-                jp_num[e.tail] = i++;
+            if (jp_num[e.head] == inf)
+                jp_num[e.head] = i++;
         }
         std::transform(begin(msf), end(msf), begin(rmq_weights), end(rmq_weights), [](const auto &e) { return e.weight; });
         RangeMaximumQuery<algen::Weight> rmq(rmq_weights);
-        std::remove_copy_if(begin(edge_list), end(edge_list) std::back_inserter(msf),
-                            [](const auto &e) { return e.weight >= rmq.query(jp_num[e.tail], jp_num[e.head]); });
+        std::remove_copy_if(begin(edge_list), end(edge_list), std::back_inserter(msf),
+                            [&rmq, &jp_num](const auto &e) { return e.weight >= rmq.query(jp_num[e.tail], jp_num[e.head]); });
         return msf;
     }
 
