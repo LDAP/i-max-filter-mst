013dab4540ad726bfda788d42b4e04c02a601e74
Lucas Alber <lucasd.alber@gmail.com>
2022-07-18 18:48:56 +0200

    AdjArray: Remove tail from edges

--- src/datastructures/adjacency_array.hpp
+++ src/datastructures/adjacency_array.hpp
@@ -5,36 +5,38 @@
 
 class AdjacencyArray {
   public:
-    using EdgeIterator = std::vector<algen::WEdge>::const_iterator;
     using NodeHandle = algen::VertexId;
+    using Edge = std::pair<algen::Weight, NodeHandle>;
+    using EdgeIterator = std::vector<Edge>::const_iterator;
 
-    explicit AdjacencyArray(const algen::WEdgeList &edges, const algen::VertexId num_vertices) {
-        _edges_sorted.resize(edges.size());
-        _indices.resize(num_vertices + 1);
-        std::partial_sort_copy(begin(edges),
-                               end(edges),
-                               begin(_edges_sorted),
-                               end(_edges_sorted),
-                               algen::TailHeadOrder<algen::WEdge>());
-
-        for (algen::VertexId v = 0, idx = 0; v < num_vertices; ++v) {
-            _indices[v] = idx;
-            while (_edges_sorted[idx].tail == v) {
-                idx++;
-            }
+    explicit AdjacencyArray(const algen::WEdgeList &edges, const algen::VertexId num_vertices)
+        : _indices(num_vertices + 1), _edges(edges.size()) {
+        std::vector<std::size_t> out_degrees(num_vertices + 1);
+        
+        for (const algen::WEdge &e : edges) {
+            out_degrees[e.tail]++;
         }
-        _indices[num_vertices] = _edges_sorted.size();
+
+        std::size_t sum = 0;
+        for (std::size_t i = 0; i < num_vertices + 1; i++) {
+            _indices[i] = sum;
+            sum += out_degrees[i];
+        }
+
+        for (const algen::WEdge &e : edges) {
+            _edges[_indices[e.tail] + --out_degrees[e.tail]] = {e.weight, e.head};
+        }
     }
 
     EdgeIterator beginEdges(const algen::VertexId v) const {
-        return _edges_sorted.begin() + _indices[v];
+        return _edges.begin() + _indices[v];
     }
 
     EdgeIterator endEdges(const algen::VertexId v) const {
-        return _edges_sorted.begin() + _indices[v + 1];
+        return _edges.begin() + _indices[v + 1];
     }
 
   private:
-    std::vector<std::size_t> _indices;
-    algen::WEdgeList _edges_sorted;
+    std::vector<NodeHandle> _indices;
+    std::vector<Edge> _edges;
 };
