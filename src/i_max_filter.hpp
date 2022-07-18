da43ade720044dabed7ee5dcab4ca40338b7fa57
Noah Wahl <noah.ares@yahoo.de>
2022-07-18 11:24:54 +0200

    IMaxFilter: treat special case and new JP interface

--- src/i_max_filter.hpp
+++ src/i_max_filter.hpp
@@ -12,28 +12,54 @@ class IMaxFilter {
         const std::size_t sample_size = std::sqrt(edge_list.size() * num_vertices);
         algen::WEdgeList edge_sample;
         edge_sample.reserve(sample_size);
+        algen::WEdgeList msf;
+        algen::VertexArray isolated_vertices;
+        std::vector<std::size_t> component_ids(num_vertices, 0);
         JarnikPrim jp;
+
+        // MST from sampled edges
         sample_edges(edge_list, edge_sample, sample_size, 42);
-        algen::WEdgeList msf = jp(edge_sample, num_vertices);
-        std::vector<algen::Weight> rmq_weights(msf.size());
+        jp(edge_sample, msf, num_vertices, component_ids, isolated_vertices);
+        add_isolated_vertices(isolated_vertices, edge_list, msf);
         std::vector<std::size_t> jp_num(num_vertices, inf);
         std::size_t i = 0;
         jp_num[msf.front().tail] = i++;
         for (const auto &e : msf) {
-            if (jp_num[e.head] == inf)
-                jp_num[e.head] = i++;
+            jp_num[e.head] = i++;
         }
+
+        // RMQ setup
+        algen::WeightArray rmq_weights(msf.size());
         std::transform(begin(msf), end(msf), begin(rmq_weights), end(rmq_weights), [](const auto &e) { return e.weight; });
         RangeMaximumQuery<algen::Weight> rmq(rmq_weights);
-        std::remove_copy_if(begin(edge_list), end(edge_list), std::back_inserter(msf),
-                            [&rmq, &jp_num](const auto &e) { return e.weight >= rmq.query(jp_num[e.tail], jp_num[e.head]); });
-        return msf;
+
+        // Filter loop
+        std::remove_copy_if(begin(edge_list),
+                            end(edge_list),
+                            std::back_inserter(msf),
+                            [&rmq, &jp_num, &component_ids](const auto &e) {
+                                return component_ids[e.tail] == component_ids[e.head] &&
+                                       e.weight >= rmq.query(jp_num[e.tail], jp_num[e.head]);
+                            });
+
+        // Final MST
+        algen::WEdgeList final_mst;
+        jp(msf, final_mst, num_vertices, component_ids, isolated_vertices);
+        return final_mst;
     }
 
     void sample_edges(const algen::WEdgeList &edges,
-                      algen::WEdgeList out,
+                      algen::WEdgeList &out,
                       const std::size_t sample_size,
                       const std::size_t seed) {
         std::sample(begin(edges), end(edges), std::back_inserter(out), sample_size, std::mt19937{seed});
+    }
+
+    void add_isolated_vertices(const algen::VertexArray &isolated_vertices,
+                               const algen::WEdgeList &edges,
+                               algen::WEdgeList &msf) {
+        std::transform(begin(isolated_vertices), end(isolated_vertices), std::back_inserter(msf), [&edges](const auto &v) {
+            return *std::find_if(begin(edges), end(edges), [v](const auto &e) { return e.tail == v; });
+        });
     }
 };
